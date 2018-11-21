66abbfa28a23b7580e34e9b044c9ab1c73ec515d
xPand4B <xpand.4beatz@gmail.com>
2018-11-21 11:56:29 +0100

    Activated error output

--- src/app/init.php
+++ src/app/init.php
@@ -1,6 +1,6 @@
 <?php
-// error_reporting(E_ALL);
-// ini_set('display_errors', 1);
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
 
 // Load composer-autoload
 require_once __DIR__.'/../vendor/autoload.php';
