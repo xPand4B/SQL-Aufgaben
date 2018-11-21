235ba5e3add72887ce5ab09dc3f81b73edb1c41b
xPand4B <xpand.4beatz@gmail.com>
2018-11-21 11:56:44 +0100

    Removed JSONData use statement

--- src/public/index.php
+++ src/public/index.php
@@ -4,7 +4,6 @@ require_once __DIR__.'/../app/init.php';
 
 // Include Namespaces
 use App\Core\App;
-use App\FileSystem\JSONData;
 
 // Create application instances
-$app  = new App();
+$app  = new App;
